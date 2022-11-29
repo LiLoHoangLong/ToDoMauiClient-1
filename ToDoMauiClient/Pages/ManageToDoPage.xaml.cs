ff1b9da76fa1755fa42e9cd38dae85c6929c7681
LongNguyen <hoanglong20041@gmail.com>
2022-11-29 08:10:39 +0700

    Revert "apimaui"
    This reverts commit ecfd0d16da8dffd33de1f0bbe4c6c128144951fc.

--- ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
+++ ToDoMauiClient/Pages/ManageToDoPage.xaml.cs
@@ -1,6 +1,5 @@
 using ToDoMauiClient.DataServices;
 using ToDoMauiClient.Models;
-using System.Diagnostics;
 
 namespace ToDoMauiClient.Pages;
 
@@ -11,7 +10,7 @@ public partial class ManageToDoPage : ContentPage
 {
     private readonly IRestDataService _dataService;
 	ToDo _toDo;
-	bool _isNew = true;
+	bool _isNew;
 
 	public ToDo ToDo
 	{
@@ -38,30 +37,7 @@ public partial class ManageToDoPage : ContentPage
 			return true;
 		return false;
 	}
-	async void OnSaveButtonClicked(object sender, EventArgs e)
-	{
-		if (_isNew)
-		{
-			Debug.WriteLine("---> Add new Item");
-			await _dataService.AddToDoAsync(ToDo);
-		}
-		else
-		{
-			Debug.WriteLine("---> Update an Item");
-			await _dataService.UpdateToDoAsync(ToDo);
-		}
-		await Shell.Current.GoToAsync("..");
-	}
 
-	async void OnDeleteButtonClicked(object sender, EventArgs e)
-	{
-		await _dataService.DeleteToDoAsync(ToDo.Id);
-        await Shell.Current.GoToAsync("..");
-    }
 
-	async void OnCancelButtonClicked(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync("..");
-	}
 
 }
